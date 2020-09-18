be492e48ffd0db5aff75a3b6f034d54a2c882fd8
JCZ-020 <jasoncruz_rocks@yahoo.com>
2020-09-17 23:02:24 -0400

    boilerplate stuff added, dependencies installed saved

---
+++ server.js
@@ -0,0 +1,30 @@
+// Bring in dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+// Morgan the logger middleware bro
+const morgan = require("morgan");
+
+
+const PORT = process.env.PORT || 9000;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// MongoDB Setup
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log("App listening on Port: ", PORT)
+});
