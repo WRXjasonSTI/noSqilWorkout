d84300e55dcfdf985532ee17336ed03bc9913056
JCZ-020 <jasoncruz_rocks@yahoo.com>
2020-09-20 17:01:09 -0400

    models and stuff, api routes worked out

---
+++ models/models.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "What kind of workout was performed (Required)",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "What was the exercise (Required)",
+        },
+        duration: {
+          type: Number,
+          required: "How long did you exercise?",
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
